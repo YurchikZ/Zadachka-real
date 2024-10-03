4e1b594b0c1f1eb763f4622ff77803322b70f1fe
st53 <st53@WS53-5>
2024-10-03 11:56:54 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadachka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
